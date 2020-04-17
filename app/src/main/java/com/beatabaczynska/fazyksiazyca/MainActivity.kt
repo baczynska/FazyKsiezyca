0a57004fe27ff66e7b4d513b3f7de19f74a1cbc6
Beata <rrrr@vp.pl>
2020-04-17 11:18:47 +0200

    from git bash

--- app/src/main/java/com/beatabaczynska/fazyksiazyca/MainActivity.kt
+++ app/src/main/java/com/beatabaczynska/fazyksiazyca/MainActivity.kt
@@ -3,10 +3,24 @@ package com.beatabaczynska.fazyksiazyca
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Integer.parseInt
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    fun julday(year: Int, month: Int, day: Int) {
+        if (year < 0) { year ++; }
+        var jy = parseInt(year);
+        var jm = parseInt(month) +1;
+        if (month <= 2) {jy--;	jm += 12;	}
+        var jul = Math.floor(365.25 *jy) + Math.floor(30.6001 * jm) + parseInt(day) + 1720995;
+        if (day+31*(month+12*year) >= (15+31*(10+12*1582))) {
+            ja = Math.floor(0.01 * jy);
+            jul = jul + 2 - ja + Math.floor(0.25 * ja);
+        }
+        return jul;
+    }
 
     fun Trig2(year: Int, month: Int, day: Int){
 
@@ -16,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         val t2 = t * t
         val a = 359.2242 + 29.105356 * n
         val am = 306.0253 + 385.816918 * n + 0.010730 * t2
-        val xtra = 0.75933 + 1.53058868 * n + (1.178e-4 - 1.55e-7 * t) * t2
+        var xtra = 0.75933 + 1.53058868 * n + (1.178e-4 - 1.55e-7 * t) * t2
         xtra += (0.1734 - 3.93e-4 * t) * Math.sin(RAD * a) - 0.4068 * Math.sin(
             RAD * am
         )
